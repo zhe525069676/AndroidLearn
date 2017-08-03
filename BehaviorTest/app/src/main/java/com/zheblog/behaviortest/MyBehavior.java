e334e04ad90ef01584b8df930c242a5272283e23
liuz <nh_zhe@163.com>
2017-08-03 16:29:57 +0800

    Behavior Test

---
+++ BehaviorTest/app/src/main/java/com/zheblog/behaviortest/MyBehavior.java
@@ -0,0 +1,18 @@
+package com.zheblog.behaviortest;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+
+/**
+ * Created by liuz on 17/8/3.
+ */
+
+public class MyBehavior extends CoordinatorLayout.Behavior {
+
+    public MyBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+
+}
